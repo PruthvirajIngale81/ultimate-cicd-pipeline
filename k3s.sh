a6ec8002d9a9cc667ff2b63764281d2eebb170d0
Pruthviraj Ingale <pruthvirajingale0081@gmail.com>
2026-02-23 18:52:37 +0530

    crucial changes

--- k3s.sh
+++ k3s.sh
@@ -20,6 +20,8 @@ kubectl apply -f k8s/mysql-deployment.yaml
 kubectl apply -f k8s/todo-deployment.yaml
 kubectl apply -f k8s/todo-service.yaml
 kubectl apply -f k8s/ingress.yaml
+
+
 # kubectl apply -f k8s/test-rate-limit.yaml
 # kubectl apply -f k8s/suricata-config.yaml
 # kubectl apply -f k8s/suricata-daemonset.yaml
@@ -30,40 +32,50 @@ kubectl apply -f k8s/ingress.yaml
 # kubectl apply -f k8s/grafana-service.yaml
 
 # add the prometheus helm repo
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-helm repo update
-#create namespace
-kubectl create ns monitoring
-# install prometheus from the helm repo
-helm install prometheus prometheus-community/prometheus -n monitoring
+# helm repo update
+# #create namespace
+# kubectl create ns monitoring || true  
+# # install prometheus from the helm repo
+# helm install prometheus prometheus-community/prometheus -n monitoring
 
-# update the prometheus service's type to NodePort
-# kubectl edit service/prometheus-server
-kubectl patch svc prometheus-server -n monitoring -p '{"spec":{"type":"NodePort"}}'
-## grafana
-helm repo add grafana https://grafana.github.io/helm-charts
+# # update the prometheus service's type to NodePort
+# # kubectl edit service/prometheus-server
+# kubectl patch svc prometheus-server -n monitoring -p '{"spec":{"type":"NodePort"}}'
+# ## grafana
+# helm repo add grafana https://grafana.github.io/helm-charts
 
-# update the repo
-helm repo update
+# # update the repo
+# helm repo update
 
-# install grafana
-helm install grafana grafana/grafana -n monitoring
+# # install grafana
+# helm install grafana grafana/grafana -n monitoring
 
-# update the grafana service's type to NodePort
-# kubectl edit service/grafana
-kubectl patch svc grafana -n monitoring   -p '{"spec":{"type":"NodePort"}}'
+# # update the grafana service's type to NodePort
+# # kubectl edit service/grafana
+# kubectl patch svc grafana -n monitoring   -p '{"spec":{"type":"NodePort"}}'
 
-# find the grafana admin password
-# copy admin password from the file
-# kubectl edit secret grafana
-kubectl patch secret grafana -n monitoring  -p '{"data":{"admin-password":"YWRtaW5AMTIz"}}'
+# # find the grafana admin password
+# # copy admin password from the file
+# # kubectl edit secret grafana
+# kubectl patch secret grafana -n monitoring  -p '{"data":{"admin-password":"YWRtaW5AMTIz"}}'
+
+
+
+
+echo "made by pruthviraj ingale all alone "
+echo "all rights reserved "
+echo " tiger group , sunbeam "
+
+if kubectl get ns monitoring >/dev/null 2>&1; then
+  echo "[INFO] Monitoring stack already installed"
+else
+  echo "[INFO] Installing monitoring stack"
+  bash monitor.sh
+fi
 
 sleep 20
 
 kubectl get all -n todo
 kubectl get all -n monitoring
-
-echo "made by pruthviraj ingale all alone "
-echo "all rights reserved "
-echo " tiger group , sunbeam "
